5a8123a3e5c8fbe977678eda1d9c358511e65615
Your Name <you@example.com>
2025-12-23 15:10:43 +0800

    first commit

---
+++ C/lx1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <math.h>
+int main() {
+printf("【欢迎】这是一个判断质数的程序,请输入一个不小于1的正整数。\n");
+int num;
+scanf("%d", &num);
+if (num == 0 || num == 1) {
+printf("【ERROR】不要逗我!\n");
+return 0;
+}
+int is_prime = 1;
+int i;
+for (i = 2; i <= sqrt(num); i++) {
+if (num % i == 0) {
+is_prime = 0;
+break;
+}
+}
+if (is_prime) {
+printf("这是一个质数\n");
+} else {
+printf("这是一个合数\n");
+}
+return 0;}
